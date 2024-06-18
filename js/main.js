64672a458868275f21bbb489966e661fd4349ac5
danil <potekhindaniil@mail.ru>
2024-06-18 09:47:12 +0700

    fixes 2

--- js/main.js
+++ js/main.js
@@ -408,6 +408,15 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-console.log('aaaa')
+window.addEventListener("DOMContentLoaded", function (){
+    let btn = document.querySelector('.play-button')
+    let videoImage = document.querySelector('.video-image')
+    let videoFrame = document.querySelector('.video-frame')
+    btn.addEventListener('click', (e) => {
+        videoImage.classList.add('none')
+        videoFrame.classList.remove('none')
+        btn.classList.add('none')
+    })
+})
 
 
--- js/videoScript.js
+++ js/videoScript.js
@@ -1,4 +1,4 @@
-(function (){
+window.addEventListener("DOMContentLoaded", function () {
     let btn = document.querySelector('.play-button')
     let videoImage = document.querySelector('.video-image')
     let videoFrame = document.querySelector('.video-frame')
@@ -7,4 +7,25 @@
         videoFrame.classList.remove('none')
         btn.classList.add('none')
     })
-})();
+
+    const menu = document.querySelector('.menuBurger');
+    const menuItems = document.querySelectorAll('.menuItem');
+    const hamburger = document.querySelector('.header-burger-button');
+    const closeIcon = document.querySelector('.closeBurgerMenu');
+    const menuIcon = document.querySelector('.openBurgerMenu');
+
+    function toggleMenu() {
+        if (menu.classList.contains('showMenu')) {
+            menu.classList.remove('showMenu');
+            closeIcon.style.display = 'none';
+            menuIcon.style.display = 'block';
+        } else {
+            menu.classList.add('showMenu');
+            closeIcon.style.display = 'block';
+            menuIcon.style.display = 'none';
+        }
+    }
+
+    hamburger.addEventListener('click', toggleMenu);
+})
+
